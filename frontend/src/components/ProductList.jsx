1c7b304e6badfea06462d430841ab3085bd7d314
Shafiul Alam <shafiulalam0307@gmail.com>
2025-08-14 10:36:58 +0600

    add modal for viewing a particular product

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -15,7 +15,7 @@ function ProductList() {
     return (
         <div>
             {/* Product Grid */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4 bg-gray-50 min-h-30">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 p-4 bg-gray-50 min-h-30">
                 {products.map((product) => (
                     <div
                         key={product._id}
